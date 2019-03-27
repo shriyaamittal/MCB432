6abdb9e1853f607ba14493a0d3a0256cf0de39c4
shriyaamittal <mittalshriyaa@gmail.com>
2019-03-27 12:08:30 -0500

    midterm project pcoa pending

--- midterm_project_#2/3.B2.sh
+++ midterm_project_#2/3.B2.sh
@@ -1,7 +1,29 @@
-awk 'BEGIN{FS="[\:]*[ ]*";gsub(/\"/,"")}$(NF)>max{max=$(NF)}/\*/{nam[$(NF)]=substr($1,2,length($1)-2)}{ct[$(NF)]++} END{for(i=1;i<=max;i++){print i,nam[i],ct[i],sum=sum+ct[i]}}' < all_otu.txt >all_otu_count.txt
+#awk 'BEGIN{FS="[\:]*[ ]*";gsub(/\"/,"")}$(NF)>max{max=$(NF)}/\*/{nam[$(NF)]=substr($1,2,length($1)-2)}{ct[$(NF)]++} END{for(i=1;i<=max;i++){print i,nam[i],ct[i],sum=sum+ct[i]}}' < all_otu.txt >all_otu_count.txt
 
-awk 'BEGIN{FS="[\:]*[ ]*"}$(NF)>max{max=$(NF)}/\*/{nam[$(NF)]=substr($1,2,length($1)-2)}{ct[$(NF)]++} END{for(i=1;i<=max;i++){print i,nam[i],ct[i],sum=sum+ct[i]}}' < all_otu.txt|sort -k3nr >all_otu_count.txt
+#awk 'BEGIN{FS="[\:]*[ ]*"}$(NF)>max{max=$(NF)}/\*/{nam[$(NF)]=substr($1,2,length($1)-2)}{ct[$(NF)]++} END{for(i=1;i<=max;i++){print i,nam[i],ct[i],sum=sum+ct[i]}}' < all_otu.txt|sort -k3nr >all_otu_count.txt
 ## This file is used is to plot OTU abundance.
 
+## Create split txt files
+#while read line;
+#do
+#	arrIN=(${line//./ })
+#	echo $arrIN
+#	echo "otu."$arrIN".txt"
+#	touch "otu."$arrIN".txt"
+#done<listFasta
+
+## Populate split txt files
+#while read line;
+#do
+#	arrIN=(${line// / })
+#	arrIN2=(${arrIN//\"/ })
+#	arrIN3=(${arrIN2//_/ })
+#	fnam="otu."${arrIN3[0]}"_"${arrIN3[1]}"_"${arrIN3[3]}".txt"
+#	echo $line >> ${fnam}
+#done<all_otu.txt
+
+ls otu.*.txt|awk '{gsub(/\.txt/,"");print}'|while read p; do echo $p;awk 'BEGIN{FS="[\:]*[ ]*"}$(NF)>max{max=$(NF)}/\*/{nam[$(NF)]=substr($1,2,length($1)-2)}{ct[$(NF)]++}END{for(i=1;i<=max;i++){if(ct[i]>0){print i,ct[i],sum=sum+ct[i]}}}' < $p.txt|sort -k2nr >$p.count.txt;done
+
+rm otu.x__.count.txt otu.x__.txt x__.txt
 
 
---
+++ midterm_project_#2/4.R2.R
@@ -0,0 +1,122 @@
+all_otu_count<- read.table("all_otu_count.txt")
+colnames(all_otu_count)<-c("OTU","name","all","ct")
+
+otu.1.count<- read.table("otu.AF_A_2.count.txt")
+colnames(otu.1.count)<-c("OTU","set1","ct")
+
+otu.2.count<- read.table("otu.AF_B_2.count.txt")
+colnames(otu.2.count)<-c("OTU","set2","ct")
+
+otu.3.count<- read.table("otu.AM_A_4.count.txt")
+colnames(otu.3.count)<-c("OTU","set3","ct")
+
+otu.4.count<- read.table("otu.AM_B_4.count.txt")
+colnames(otu.4.count)<-c("OTU","set4","ct")
+
+otu.5.count<- read.table("otu.AQ_A_1.count.txt")
+colnames(otu.5.count)<-c("OTU","set5","ct")
+
+otu.6.count<- read.table("otu.AQ_B_1.count.txt")
+colnames(otu.6.count)<-c("OTU","set6","ct")
+
+otu.7.count<- read.table("otu.AX_A_2.count.txt")
+colnames(otu.7.count)<-c("OTU","set7","ct")
+
+otu.8.count<- read.table("otu.AX_B_2.count.txt")
+colnames(otu.8.count)<-c("OTU","set8","ct")
+
+otu.9.count<- read.table("otu.BB_A_3.count.txt")
+colnames(otu.9.count)<-c("OTU","set9","ct")
+
+otu.10.count<- read.table("otu.BB_B_3.count.txt")
+colnames(otu.10.count)<-c("OTU","set10","ct")
+
+otu.11.count<- read.table("otu.BF_A_3.count.txt")
+colnames(otu.11.count)<-c("OTU","set11","ct")
+
+otu.12.count<- read.table("otu.BF_B_3.count.txt")
+colnames(otu.12.count)<-c("OTU","set12","ct")
+
+otu.13.count<- read.table("otu.BI_A_3.count.txt")
+colnames(otu.13.count)<-c("OTU","set13","ct")
+
+otu.14.count<- read.table("otu.BI_B_3.count.txt")
+colnames(otu.14.count)<-c("OTU","set14","ct")
+
+otu.15.count<- read.table("otu.BM_A_4.count.txt")
+colnames(otu.15.count)<-c("OTU","set15","ct")
+
+otu.16.count<- read.table("otu.BM_B_4.count.txt")
+colnames(otu.16.count)<-c("OTU","set16","ct")
+
+otu.17.count<- read.table("otu.BU_A_4.count.txt")
+colnames(otu.17.count)<-c("OTU","set17","ct")
+
+otu.18.count<- read.table("otu.BU_B_4.count.txt")
+colnames(otu.18.count)<-c("OTU","set18","ct")
+
+otu.19.count<- read.table("otu.BX_A_3.count.txt")
+colnames(otu.19.count)<-c("OTU","set19","ct")
+
+otu.20.count<- read.table("otu.BX_B_3.count.txt")
+colnames(otu.20.count)<-c("OTU","set20","ct")
+
+m1<-merge(x=all_otu_count,y=otu.1.count,by="OTU", all.x=T, all.y=T)
+m1[is.na(m1)]<-0; m1[,6]<-NULL; m1[,4]<-NULL; head(m1)
+
+otu.2.count[,3]<-NULL
+otu.3.count[,3]<-NULL
+otu.4.count[,3]<-NULL
+otu.5.count[,3]<-NULL
+otu.6.count[,3]<-NULL
+otu.7.count[,3]<-NULL
+otu.8.count[,3]<-NULL
+otu.9.count[,3]<-NULL
+otu.10.count[,3]<-NULL
+otu.11.count[,3]<-NULL
+otu.12.count[,3]<-NULL
+otu.13.count[,3]<-NULL
+otu.14.count[,3]<-NULL
+otu.15.count[,3]<-NULL
+otu.16.count[,3]<-NULL
+otu.17.count[,3]<-NULL
+otu.18.count[,3]<-NULL
+otu.19.count[,3]<-NULL
+otu.20.count[,3]<-NULL
+
+m2<-merge(x=m1,y=otu.2.count,by="OTU", all.x=T, all.y=T);m2[is.na(m2)]<-0;head(m2)
+m3<-merge(x=m2,y=otu.3.count,by="OTU", all.x=T, all.y=T);m3[is.na(m3)]<-0;head(m3)
+m4<-merge(x=m3,y=otu.4.count,by="OTU", all.x=T, all.y=T);m4[is.na(m4)]<-0;head(m4)
+m5<-merge(x=m4,y=otu.5.count,by="OTU", all.x=T, all.y=T);m5[is.na(m5)]<-0;head(m5)
+m6<-merge(x=m5,y=otu.6.count,by="OTU", all.x=T, all.y=T);m6[is.na(m6)]<-0;head(m6)
+m7<-merge(x=m6,y=otu.7.count,by="OTU", all.x=T, all.y=T);m7[is.na(m7)]<-0;head(m7)
+m8<-merge(x=m7,y=otu.8.count,by="OTU", all.x=T, all.y=T);m8[is.na(m8)]<-0;head(m8)
+m9<-merge(x=m8,y=otu.9.count,by="OTU", all.x=T, all.y=T);m9[is.na(m9)]<-0;head(m9)
+m10<-merge(x=m9,y=otu.10.count,by="OTU", all.x=T, all.y=T);m10[is.na(m10)]<-0;head(m10)
+m11<-merge(x=m10,y=otu.11.count,by="OTU", all.x=T, all.y=T);m11[is.na(m11)]<-0;head(m11)
+m12<-merge(x=m11,y=otu.12.count,by="OTU", all.x=T, all.y=T);m12[is.na(m12)]<-0;head(m12)
+m13<-merge(x=m12,y=otu.13.count,by="OTU", all.x=T, all.y=T);m13[is.na(m13)]<-0;head(m13)
+m14<-merge(x=m13,y=otu.14.count,by="OTU", all.x=T, all.y=T);m14[is.na(m14)]<-0;head(m14)
+m15<-merge(x=m14,y=otu.15.count,by="OTU", all.x=T, all.y=T);m15[is.na(m15)]<-0;head(m15)
+m16<-merge(x=m15,y=otu.16.count,by="OTU", all.x=T, all.y=T);m16[is.na(m16)]<-0;head(m16)
+m17<-merge(x=m16,y=otu.17.count,by="OTU", all.x=T, all.y=T);m17[is.na(m17)]<-0;head(m17)
+m18<-merge(x=m17,y=otu.18.count,by="OTU", all.x=T, all.y=T);m18[is.na(m18)]<-0;head(m18)
+m19<-merge(x=m18,y=otu.19.count,by="OTU", all.x=T, all.y=T);m19[is.na(m19)]<-0;head(m19)
+m20<-merge(x=m19,y=otu.20.count,by="OTU", all.x=T, all.y=T);m20[is.na(m20)]<-0;head(m20)
+
+write.table(m20,"otu_table.txt")
+
+
+OTU<-m20[,-2]
+OTU_table<-OTU[,-1]
+OTU_tablex<-OTU_table[,-1]; head(OTU_tablex)
+write.csv(OTU_tablex,"otu_tablex.csv")
+
+library(ape)
+library(vegan)
+
+OTU.dist<- vegdist(OTU)
+Sys.time();OTU.pcoa<- pcoa(OTU.dist);Sys.time();
+OTU.pcoa
+
+biplot(OTU.pcoa)
