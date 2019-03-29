dd1a73e8f200de824c98105efdf90fbb1468b9d3
Shriyaa Mittal <mittalshriyaa@gmail.com>
2019-03-29 16:39:24 -0500

    Update 3.B2.sh

--- midterm_project_#2/3.B2.sh
+++ midterm_project_#2/3.B2.sh
@@ -4,23 +4,23 @@
 ## This file is used is to plot OTU abundance.
 
 ## Create split txt files
-#while read line;
-#do
-#	arrIN=(${line//./ })
-#	echo $arrIN
-#	echo "otu."$arrIN".txt"
-#	touch "otu."$arrIN".txt"
-#done<listFasta
+while read line;
+do
+	arrIN=(${line//./ })
+	echo $arrIN
+	echo "otu."$arrIN".txt"
+	touch "otu."$arrIN".txt"
+done<listFasta
 
 ## Populate split txt files
-#while read line;
-#do
-#	arrIN=(${line// / })
-#	arrIN2=(${arrIN//\"/ })
-#	arrIN3=(${arrIN2//_/ })
-#	fnam="otu."${arrIN3[0]}"_"${arrIN3[1]}"_"${arrIN3[3]}".txt"
-#	echo $line >> ${fnam}
-#done<all_otu.txt
+while read line;
+do
+	arrIN=(${line// / })
+	arrIN2=(${arrIN//\"/ })
+	arrIN3=(${arrIN2//_/ })
+	fnam="otu."${arrIN3[0]}"_"${arrIN3[1]}"_"${arrIN3[3]}".txt"
+	echo $line >> ${fnam}
+done<all_otu.txt
 
 ls otu.*.txt|awk '{gsub(/\.txt/,"");print}'|while read p; do echo $p;awk 'BEGIN{FS="[\:]*[ ]*"}$(NF)>max{max=$(NF)}/\*/{nam[$(NF)]=substr($1,2,length($1)-2)}{ct[$(NF)]++}END{for(i=1;i<=max;i++){if(ct[i]>0){print i,ct[i],sum=sum+ct[i]}}}' < $p.txt|sort -k2nr >$p.count.txt;done
 
