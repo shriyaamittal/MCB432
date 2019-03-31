680b718abca9b594c1c1752ab46dbbbde92ca59b
shriyaamittal <mittalshriyaa@gmail.com>
2019-03-31 18:05:58 -0500

    ls *_*_?.fasta > listFasta

--- midterm_project_#2/3.B2.sh
+++ midterm_project_#2/3.B2.sh
@@ -1,7 +1,9 @@
-#awk 'BEGIN{FS="[\:]*[ ]*";gsub(/\"/,"")}$(NF)>max{max=$(NF)}/\*/{nam[$(NF)]=substr($1,2,length($1)-2)}{ct[$(NF)]++} END{for(i=1;i<=max;i++){print i,nam[i],ct[i],sum=sum+ct[i]}}' < all_otu.txt >all_otu_count.txt
+awk 'BEGIN{FS="[\:]*[ ]*";gsub(/\"/,"")}$(NF)>max{max=$(NF)}/\*/{nam[$(NF)]=substr($1,2,length($1)-2)}{ct[$(NF)]++} END{for(i=1;i<=max;i++){print i,nam[i],ct[i],sum=sum+ct[i]}}' < all_otu.txt >all_otu_count.txt
 
-#awk 'BEGIN{FS="[\:]*[ ]*"}$(NF)>max{max=$(NF)}/\*/{nam[$(NF)]=substr($1,2,length($1)-2)}{ct[$(NF)]++} END{for(i=1;i<=max;i++){print i,nam[i],ct[i],sum=sum+ct[i]}}' < all_otu.txt|sort -k3nr >all_otu_count.txt
+awk 'BEGIN{FS="[\:]*[ ]*"}$(NF)>max{max=$(NF)}/\*/{nam[$(NF)]=substr($1,2,length($1)-2)}{ct[$(NF)]++} END{for(i=1;i<=max;i++){print i,nam[i],ct[i],sum=sum+ct[i]}}' < all_otu.txt|sort -k3nr >all_otu_count.txt
 ## This file is used is to plot OTU abundance.
+
+ls *_*_?.fasta > listFasta
 
 ## Create split txt files
 while read line;
