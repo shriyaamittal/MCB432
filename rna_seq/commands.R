65d2ec088a8ccc00aab6b6290ff7b6e15c810ee7
Shriyaa Mittal <mittalshriyaa@gmail.com>
2019-04-30 10:36:00 -0500

    Update commands.R

--- rna_seq/commands.R
+++ rna_seq/commands.R
@@ -115,3 +115,66 @@ boxplot(lcpm,las=2,col=col,main="")
 title(main="B. Example: Normalised data",ylab="Log-cpm")
 
 
+## Figure 3
+
+cpm<-cpm(x,log=TRUE)
+par(mfrow=c(1,2))
+col.group<-group
+levels(col.group)<-brewer.pal(nlevels(col.group),"Set1")
+col.group<-as.character(col.group)
+col.lane<-lane
+levels(col.lane)<-brewer.pal(nlevels(col.lane),"Set2")
+col.lane<-as.character(col.lane)
+plotMDS(lcpm,labels=group,col=col.group)
+title(main="A. Sample groups")
+plotMDS(lcpm,labels=lane,col=col.lane,dim=c(3,4))
+title(main="B. Sequencing lanes")
+
+
+design<-model.matrix(~0+group+lane)
+colnames(design)<-gsub("group","",colnames(design))
+design
+
+contr.matrix<-makeContrasts(
+	BasalvsLP= Basal-LP,
+	BasalvsML= Basal - ML,
+	LPvsML= LP - ML,
+	levels=colnames(design))
+contr.matrix
+
+## Figure 4
+v<-voom(x, design,plot=TRUE)
+vfit<-lmFit(v, design)
+vfit<-contrasts.fit(vfit,contrasts=contr.matrix)
+efit<-eBayes(vfit)
+plotSA(efit)
+
+
+summary(decideTests(efit))
+tfit<-treat(vfit,lfc=1)
+dt<-decideTests(tfit)
+summary(dt)
+
+de.common<-which(dt[,1]!=0 & dt[,2]!=0)
+length(de.common)
+head(tfit$genes$SYMBOL[de.common],n=20)
+
+## Figure 5
+vennDiagram(dt[,1:2],circle.col=c("turquoise","salmon"))
+write.fit(tfit, dt,file="results.txt")
+
+basal.vs.lp<-topTreat(tfit,coef=1,n=Inf)
+basal.vs.ml<-topTreat(tfit,coef=2,n=Inf)
+
+## Figure 6
+plotMD(tfit,column=1,status=dt[,1],main=colnames(tfit)[1],xlim=c(-8,13))
+## glMDPlot(tfit,coef=1,status=dt[,1],main=colnames(tfit)[1],counts=x$counts,samples=colnames(x),anno=x$genes,groups=group,id.column="ENTREZID",display.columns=c("SYMBOL","ENTREZID"),search.by="SYMBOL",launch=FALSE)
+
+
+library(gplots)
+
+## Figure 7
+basal.vs.lp.topgenes<-basal.vs.lp$ENTREZID[1:100]
+i<-which(v$genes$ENTREZID %in% basal.vs.lp.topgenes)
+mycol<-colorpanel(1000,"blue","white","red")
+heatmap.2(v$E[i,],scale="row",labRow=v$genes$SYMBOL[i],labCol=group,   col=mycol,trace="none",density.info="none",   margin=c(8,6),lhei=c(2,10),dendrogram="column")
